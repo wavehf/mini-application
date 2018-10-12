e658f7bdb9468b87c150d37bc9a56e5203ab63d3
wavehf <zhangbo357@126.com>
2018-10-12 19:29:57 +0800

    5-3

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,94 @@
-Page({})
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
+Page({
+  data: {
+    nowTemp: "",
+    nowWeather: "",
+    nowWeatherBackground: "",
+    hourlyWeather: [],
+    todayTemp: "",
+    todayDate: ""
+  },
+  onPullDownRefresh() {
+    this.getNow(() => {
+      wx.stopPullDownRefresh()
+    })
+  },
+  onLoad() {
+    this.getNow()
+  },
+  getNow(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now', 
+      data: {
+        city:'广州市'
+      },
+      success:res => {
+        console.log(res)
+        let result = res.data.result
+        this.setNow(result)
+        this.setHourlyWeather(result)
+        this.setToday(result)
+      },
+      complete: () => {
+        callback && callback()
+      },
+      
+    })
+  },
+  setNow(result) {
+    let temp = result.now.temp
+    let weather = result.now.weather
+
+    console.log(temp, weather)
+    this.setData({
+      nowTemp: temp + "°",
+      nowWeather: weatherMap[weather],
+      nowWeatherBackground: '/images/' + weather + '-bg.png'
+    })
+
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: weatherColorMap[weather],
+    })
+  },
+  setHourlyWeather(result) {
+    let hourlyWeather = []
+    let nowHour = new Date().getHours()
+
+    for (let i = 0; i < 8; i++) {
+      hourlyWeather.push({
+        time: (i * 3 + nowHour) % 24 + '时',
+        iconPath: '/images/' + result.forecast[i].weather + '-icon.png',
+        temp: result.forecast[i].temp + '°'
+      })
+    }
+    hourlyWeather[0].time = '现在'
+    this.setData({
+      hourlyWeather: hourlyWeather,
+    })
+  },
+  setToday(result) {
+    let date = new Date()
+    this.setData({
+      todayTemp: `${result.today.minTemp}° - ${result.today.maxTemp}°`,
+      todayDate: `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} 今天`
+    })
+  }
+})
